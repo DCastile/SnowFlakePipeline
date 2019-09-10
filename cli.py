bd84c8007f786af1eacbce3f22c4e65ffcb40a23
Derek Castile <DCastile@curvature.com>
2019-09-10 16:41:37 -0700

    switched to absolute value of checksum so we are getting other half

--- cli.py
+++ cli.py
@@ -4,6 +4,11 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='A CLI for moving data to SnowFlake')
 
+    parser.add_argument('-a', '--account', type=str, help='Define the SnowFlake account to authenticate against')
+    parser.add_argument('-u', '--user', type=str, help='Define the SnowFlake user to login with')
+    parser.add_argument('-p', '--password', type=str, help='Define the SnowFlake password to login with')
+
+
     parser.add_argument('-t', '--type', choices=['incremental', 'full'], type=str, default='incremental',
                         help='Define the data transfer type (Incremental or Full)')
     parser.add_argument('-s', '--source', choices=['sap', 'singlePoint'], type=str,
@@ -31,3 +36,7 @@ if __name__ == '__main__':
     if args.table:
         if not args.source:
             raise argparse.ArgumentError('Batch has been specified without a source')
+
+
+    # some logic for account/user/password
+
