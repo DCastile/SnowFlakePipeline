e0f320bd648d37b8d24a4dc303307d125a732af5
Derek Castile <DCastile@curvature.com>
2019-10-24 11:36:18 -0700

    update

--- misc_qrys/table metadata.sql
+++ misc_qrys/table metadata.sql
@@ -1,4 +1,4 @@
-
+use SAP_Production
 
         with table_columns as (
             select
@@ -31,7 +31,7 @@
         data as (
         select table_name, column_id, column_name, part_of_pk
         from table_columns tc
-        where exists(select 1 from view_columns vc where tc.schema_name = vc.schema_name and concat('V_', tc.table_name) = vc.view_name and tc.column_name = vc.column_name)
+--         where exists(select 1 from view_columns vc where tc.schema_name = vc.schema_name and concat('V_', tc.table_name) = vc.view_name and tc.column_name = vc.column_name)
 --         and table_name = 'TCURX'
 --         order by schema_name, table_name, column_id
 		)
@@ -58,9 +58,122 @@
 			part_of_pk,
 		    min(column_id) over(partition by table_name) min_pk_col_id
 	    from data
-		where part_of_pk = 1
+-- 		where part_of_pk = 1
 	) a
---     where min_pk_col_id != 1
+    where 1=1-- min_pk_col_id != 1
+        and table_name in (
+	'AFIH',
+	'AFKO',
+	'AFPO',
+	'AUFK',
+	'AUFM',
+	'AUSP',
+	'BKPF',
+	'BSAD',
+	'BSAK',
+	'BSEG_LC',
+	'BSID',
+	'BSIK',
+	'BSIS_BSAS_LC',
+	'CABN',
+	'CABNT',
+	'CATSDB',
+	'COAS',
+	'COBK',
+	'COEP_LC',
+	'COST',
+	'CSKS',
+	'CSKT',
+	'EBAN',
+	'EKBE',
+	'EKET',
+	'EKKN',
+	'EKKO',
+	'EKPO',
+	'EQBS',
+	'EQKT',
+	'EQUI',
+	'EQUZ',
+	'FPLA',
+	'FPLT',
+	'HRP1000',
+	'HRP1001',
+	'IHPA',
+	'KLAH',
+	'KNA1',
+	'KNB1',
+	'KNVP',
+	'KNVV',
+	'KONDD',
+	'KONDDP',
+	'KOTD001',
+	'KSML',
+	'LFA1',
+	'LFB1',
+	'LFM1',
+	'LIKP',
+	'LIPS',
+	'MAKT',
+	'MARA',
+	'MARC',
+	'MARD',
+	'MBEW_LC',
+	'MCH1',
+	'MCHB',
+	'MKPF',
+	'MSEG',
+	'MSKA',
+	'OBJK',
+	'PA0001',
+	'PA0041',
+	'PA0105',
+	'PMSDO',
+	'PTRV_SCOS',
+	'PTRV_SHDR',
+	'PTRV_SREC',
+	'PURGTX_T',
+	'RBKP',
+	'RSEG',
+	'SER01',
+	'SER02',
+	'SER03',
+	'SKAT',
+	'T001',
+	'T001L',
+	'T001W',
+	'T002T',
+	'T003',
+	'T003O',
+	'T003T',
+	'T024',
+	'T151',
+	'T151T',
+	'T156',
+	'T156T',
+	'T158W',
+	'T161T',
+	'T179T',
+	'T527X',
+	'T528B',
+	'T528T',
+	'TCURX',
+	'TVAK',
+	'TVAKT',
+	'TVAPT',
+	'TVLVT',
+	'VBAK',
+	'VBAP',
+	'VBFA',
+	'VBKD',
+	'VBPA',
+	'VBREVE',
+	'VBRK',
+	'VBRP_LC',
+	'VEDA',
+	'WYT3',
+	'ZSMSCONTA',
+	'ZTT_ZONE'
+	    )
 	order by table_name, column_id
 
 
