1b010057a5f1e8825148e7eacbd3a74577487771
Derek Castile <DCastile@curvature.com>
2019-11-11 16:14:14 -0800

    manually fixing SinglePoint.dbo.IncPriority column order

--- src_qrys/singlepoint/SinglePoint.dbo.IncPriority.sql
+++ src_qrys/singlepoint/SinglePoint.dbo.IncPriority.sql
@@ -1,6 +1,6 @@
 select
-	 [PriorityID] = quotename([IncPriority].[PriorityID], char(34)),
 	 [PriorityCodeKey] = quotename([IncPriority].[PriorityCodeKey], char(34)),
+	 [PriorityID] = quotename([IncPriority].[PriorityID], char(34)),
 	 [PriorityCodeID] = quotename([IncPriority].[PriorityCodeID], char(34)),
 	 [Description] = quotename([IncPriority].[Description], char(34))
 from SinglePoint.dbo.IncPriority
